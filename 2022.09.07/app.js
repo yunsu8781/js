7f7892b2e2e19a66c82d121a5e499c7fd5c58e0a
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2022-09-07 01:07:28 +0900

    Update app.js

--- 2022.09.07/app.js
+++ 2022.09.07/app.js
@@ -4,8 +4,12 @@ console.log(isNaN(age));
 
 if(isNaN(age)){
     console.log("숫자가 아닙니다.");
-}else if(age <= 19){
-    console.log("음주 멈춰!");
-}else if(age > 19){
+}else if(age < 18 || age < 0){
+    console.log("너무 어려요");
+}else if(age >= 18 && age <= 50){
     console.log("음주 즐겨버려");
+}else if(age > 50 && age <= 80){
+    console.log("운동합시다!");
+}else if(age > 80) {
+    console.log("원하는거 다 하시구 사세요");
 }
